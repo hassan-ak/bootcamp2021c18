386512839c0a5af773e749ab91ab9361516e0a62
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2021-12-23 05:25:41 +0500

    Update step00_neptune_lambda-stack.ts

--- step04_graph_databases/step45a_neptune_cypher_gql/step00_neptune_lambda/lib/step00_neptune_lambda-stack.ts
+++ step04_graph_databases/step45a_neptune_cypher_gql/step00_neptune_lambda/lib/step00_neptune_lambda-stack.ts
@@ -1,6 +1,7 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Tags } from 'aws-cdk-lib';
+import { aws_ec2 as ec2 } from 'aws-cdk-lib';
+import { aws_neptune as neptune } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class Step00NeptuneLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -8,9 +9,52 @@ export class Step00NeptuneLambdaStack extends Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Step00NeptuneLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // VPC
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      subnetConfiguration: [
+        {
+          cidrMask: 24, // Creates a size /24 IPv4 subnet (a range of 256 private IP addresses) in the VPC
+          name: 'Ingress',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
+
+    // Create a security group and subnetgroup to ensure lambda and neptune cluster deploy on the same vpc
+    const sgl = new ec2.SecurityGroup(this, 'mySecurityGroup', {
+      vpc,
+      allowAllOutbound: true,
+      description: 'Security Group 1',
+      securityGroupName: 'mySecurityGroup',
+    });
+    Tags.of(sgl).add('name', 'mySecurityGroup');
+    sgl.addIngressRule(sgl, ec2.Port.tcp(8182), 'MyRule');
+    const neptuneSubnet = new neptune.CfnDBSubnetGroup(
+      this,
+      'neptuneSubnetGroup',
+      {
+        dbSubnetGroupDescription: 'My Subnet',
+        subnetIds: vpc.selectSubnets({
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        }).subnetIds,
+        dbSubnetGroupName: 'mysubnetgroup',
+      }
+    );
+
+    // Creating Naptune cluster
+    const neptuneCluster = new neptune.CfnDBCluster(this, 'MyCluster', {
+      dbSubnetGroupName: neptuneSubnet.dbSubnetGroupName,
+      dbClusterIdentifier: 'myDbCluster',
+      vpcSecurityGroupIds: [sgl.securityGroupId],
+    });
+    neptuneCluster.addDependsOn(neptuneSubnet);
+
+    // Creating Naptune Instance
+    const neptuneInstance = new neptune.CfnDBInstance(this, 'myinstance', {
+      dbInstanceClass: 'db.t3.medium',
+      dbClusterIdentifier: neptuneCluster.dbClusterIdentifier,
+      availabilityZone: vpc.availabilityZones[0],
+    });
+    neptuneInstance.addDependsOn(neptuneCluster);
   }
 }
