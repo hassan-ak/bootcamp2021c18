3bebe39a61cd3ee0109e88d2152b8174bc2f7eac
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2021-12-24 01:14:39 +0500

    cdk app

---
+++ step04_graph_databases/step45a_neptune_cypher_gql/step01_CRUD_awsappsyc/test/step01_crud_awsappsyc.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Step01CrudAwsappsyc from '../lib/step01_crud_awsappsyc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step01_crud_awsappsyc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step01CrudAwsappsyc.Step01CrudAwsappsycStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
